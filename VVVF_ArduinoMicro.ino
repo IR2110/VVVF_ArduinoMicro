9c2a3ef773e5b3c14e8a6b1841c2f20b24c34b19
IR2110 <120819046+IR2110@users.noreply.github.com>
2025-08-08 23:22:03 +0900

    fix: 過変調時のキャリア周波数計算の修正
    update関数内でのキャリア周波数計算を修正。asin関数の引数をmax/minで制限し、過変調時の計算が正しく行われるように変更。

--- VVVF_ArduinoMicro.ino
+++ VVVF_ArduinoMicro.ino
@@ -43,7 +43,7 @@ void update() {  //  samplingRate [Hz]ごとに呼ばれる
   pmref.mVoltage = pmref.fSig / 60.0f + 0.02f;
   if (pm.modulation_index * (2 * 127) > 1) {
     //過変調になっても平均SW回数を一定にする
-    pmref.fCarrier = 525 * (M_PI / 2) / asin(1 / (pm.modulation_index * (2 * 127)));
+      pmref.fCarrier = 525 * (M_PI / 2) / asin(max(min(1 / (pm.modulation_index * (2 * 127)), 1), -1));
   } else {
     pmref.fCarrier = 525;
   }
