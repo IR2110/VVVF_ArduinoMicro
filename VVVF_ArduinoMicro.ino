ae726c10b88d19feba1770a5093478326e165201
IR2110 <120819046+IR2110@users.noreply.github.com>
2025-08-08 13:45:46 +0900

    324546789

--- VVVF_ArduinoMicro.ino
+++ VVVF_ArduinoMicro.ino
@@ -10,7 +10,7 @@ PulseModeReference pmref;
 // 位相アキュムレータ（DDSよろしくのアレ）
 volatile uint32_t phase_accumulator = 0;
 
-const float samplingRate = 100.0f;
+const float samplingRate = 1000.0f;
 
 void setup_timer1_3phase();
 void update_carrier_frequency();
@@ -19,95 +19,100 @@ void update_duties_and_set_ocr();
 void setup_timer3_param_update();
 
 void setup() {
-    cli();
-    pmref.mVoltage = 0.0f;
-    pmref.fCarrier = 1050.0f;
-    pmref.fSig = 0.0f;
-    pmref.SvmEnable = 0;
-    pmref.ThiEnable = 1;
-    setup_timer1_3phase();
-    setup_timer3_param_update();
-    sei();
+  cli();
+  pmref.mVoltage = 0.0f;
+  pmref.fCarrier = 1000.0f;
+  pmref.fSig = 0.0f;
+  pmref.SvmEnable = 0;
+  pmref.ThiEnable = 0;
+  setup_timer1_3phase();
+  setup_timer3_param_update();
+  sei();
 }
 
 void loop() {}
 
 void update() {  //  samplingRate [Hz]ごとに呼ばれる
-    pmref.fSig += 4 / samplingRate;
-    pmref.mVoltage = pmref.fSig / 60.0f + 0.02f;
-    UpdatePwmMode(pmref, &pm);
+  pmref.fSig += 4 / samplingRate;
+  pmref.mVoltage = pmref.fSig / 60.0f + 0.02f;
+  if (pm.modulation_index * (2 * 127) > 1) {
+    pmref.fCarrier = 525 * (M_PI / 2) / asin(1 / (pm.modulation_index * (2 * 127)));
+  } else {
+    pmref.fCarrier = 525;
+  }
+  UpdatePwmMode(pmref, &pm);
 }
 
 // タイマ1の初期化
 void setup_timer1_3phase() {
-    UpdatePwmMode(pmref, &pm);
-    update_duties_and_set_ocr();
-    USBCON &= ~(1 << USBE);  // USBマクロを無効化
-    PLLCSR = 0;              // PLLを無効化
-    UDIEN = 0;               // USB割り込みをすべて無効化
+  UpdatePwmMode(pmref, &pm);
+  update_duties_and_set_ocr();
+  USBCON &= ~(1 << USBE);  // USBマクロを無効化
+  PLLCSR = 0;              // PLLを無効化
+  UDIEN = 0;               // USB割り込みをすべて無効化
 
-    DDRB |= (1 << DDB5) | (1 << DDB6) | (1 << DDB7);  // OC1A/B/Cを出力に設定
+  DDRB |= (1 << DDB5) | (1 << DDB6) | (1 << DDB7);  // OC1A/B/Cを出力に設定
 
-    // TCCR1A/Bレジスタ設定
-    TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << COM1C1);  // 次の更新はBOTTOM（谷）
-    TCCR1B = (1 << WGM13) | (1 << CS11);                     // 位相基準PWM（TOP=ICR1）、プリスケーラ8
+  // TCCR1A/Bレジスタ設定
+  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << COM1C1);  // 次の更新はBOTTOM（谷）
+  TCCR1B = (1 << WGM13) | (1 << CS11);                     // 位相基準PWM（TOP=ICR1）、プリスケーラ8
 
-    // 割り込み許可
-    TIMSK1 = (1 << ICIE1) | (1 << TOIE1);
+  // 割り込み許可
+  TIMSK1 = (1 << ICIE1) | (1 << TOIE1);
 }
 
 // パラメータ更新用のタイマー3の設定
 void setup_timer3_param_update() {
-    // CTCモード、プリスケーラ64
-    TCCR3A = 0;
-    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+  // CTCモード、プリスケーラ64
+  TCCR3A = 0;
+  TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
 
-    // (16000000 / 64) / 100Hz = 2500
-    OCR3A = (uint16_t)((F_CPU / 64) / samplingRate);
+  // (16000000 / 64) / 100Hz = 2500
+  OCR3A = (uint16_t)((F_CPU / 64) / samplingRate);
 
-    // タイマー3の比較A割り込みを有効化
-    TIMSK3 = (1 << OCIE3A);
+  // タイマー3の比較A割り込みを有効化
+  TIMSK3 = (1 << OCIE3A);
 }
 
 // 割り込み処理共通
 void update_duties_and_set_ocr() {
-    pwm_config pm_hold = pm;  // バッファをとっておいて、これを実行中にupdateが走ってpmが変更されても影響しないように
+  pwm_config pm_hold = pm;  // バッファをとっておいて、これを実行中にupdateが走ってpmが変更されても影響しないように
 
-    ICR1 = pm_hold.top;
+  ICR1 = pm_hold.top;
 
-    phase_accumulator += pm_hold.increment;
-    phase_accumulator &= SIN_LENGTH * SHIFT - 1;  // 剰余
+  phase_accumulator += pm_hold.increment;
+  phase_accumulator &= SIN_LENGTH * SHIFT - 1;  // 剰余
 
-    uint8_t phase_index = (uint8_t)(phase_accumulator / SHIFT);
+  uint8_t phase_index = (uint8_t)(phase_accumulator / SHIFT);
 
-    //0~top
-    uint16_t duty_u = (uint16_t)(((float)(pgm_read_byte_near(&SIN_U[pm_hold.sig_mode][phase_index]) - LUT_ZERO_LEVEL) * pm_hold.modulation_index + 0.5f) * pm_hold.top);
-    uint16_t duty_v = (uint16_t)(((float)(pgm_read_byte_near(&SIN_V[pm_hold.sig_mode][phase_index]) - LUT_ZERO_LEVEL) * pm_hold.modulation_index + 0.5f) * pm_hold.top);
-    uint16_t duty_w = (uint16_t)(((float)(pgm_read_byte_near(&SIN_W[pm_hold.sig_mode][phase_index]) - LUT_ZERO_LEVEL) * pm_hold.modulation_index + 0.5f) * pm_hold.top);
+  //0~top
+  float duty_u = (((float)(pgm_read_byte_near(&SIN_U[pm_hold.sig_mode][phase_index]) - LUT_ZERO_LEVEL) * pm_hold.modulation_index + 0.5f));
+  float duty_v = (((float)(pgm_read_byte_near(&SIN_V[pm_hold.sig_mode][phase_index]) - LUT_ZERO_LEVEL) * pm_hold.modulation_index + 0.5f));
+  float duty_w = (((float)(pgm_read_byte_near(&SIN_W[pm_hold.sig_mode][phase_index]) - LUT_ZERO_LEVEL) * pm_hold.modulation_index + 0.5f));
 
-    // 過変調してもタイマがぶっこわれないように制限
-    duty_u = max(min(duty_u, pm_hold.top), 0);
-    duty_v = max(min(duty_v, pm_hold.top), 0);
-    duty_w = max(min(duty_w, pm_hold.top), 0);
+  // 過変調してもタイマがぶっこわれないように制限
+  duty_u = max(min(duty_u, 1), 0);
+  duty_v = max(min(duty_v, 1), 0);
+  duty_w = max(min(duty_w, 1), 0);
 
-    // 出力コンペアレジスタに設定
-    OCR1A = duty_u;
-    OCR1B = duty_v;
-    OCR1C = duty_w;
+  // 出力コンペアレジスタに設定
+  OCR1A = (uint16_t)(duty_u * (pm_hold.top));
+  OCR1B = (uint16_t)(duty_v * (pm_hold.top));
+  OCR1C = (uint16_t)(duty_w * (pm_hold.top));
 }
 
 // カウンタがBOTTOM（谷）に達したとき
 ISR(TIMER1_OVF_vect) {
-    update_duties_and_set_ocr();
-    TCCR1A = 0b10101010;  // 次の更新はTOP（山）
+  update_duties_and_set_ocr();
+  TCCR1A = 0b10101010;  // 次の更新はTOP（山）
 }
 
 // カウンタがTOP（山）に達したとき
 ISR(TIMER1_CAPT_vect) {
-    update_duties_and_set_ocr();
-    TCCR1A = 0b10101000;  // 次の更新はBOTTOM（谷）
+  update_duties_and_set_ocr();
+  TCCR1A = 0b10101000;  // 次の更新はBOTTOM（谷）
 }
 
 ISR(TIMER3_COMPA_vect) {  // updateをsamplingRate [Hz]で呼ぶ
-    update();
+  update();
 }
--- pwm_controller.h
+++ pwm_controller.h
@@ -13,59 +13,60 @@
 // 位相倍率(2^n)
 #define SHIFT 2097152
 
+#define MAX_FC 1500
+
 // 本番のPWM生成に使うためのちゃんと整形されたPWM設定。
 typedef struct pwm_config {
-    volatile float carrier_freq_hz;
-    volatile float signal_freq_hz;
-    volatile float modulation_index;  // 信号波振幅
-    volatile uint8_t sig_mode;
-    volatile uint16_t top;
-    volatile uint32_t increment;
+  volatile float carrier_freq_hz;
+  volatile float signal_freq_hz;
+  volatile float modulation_index;  // 信号波振幅
+  volatile uint8_t sig_mode;
+  volatile uint16_t top;
+  volatile uint32_t increment;
 } pwm_config;
 
 // PWMパターン設定から送られてくるナマのPWM設定
 typedef struct PulseModeReference {
-    volatile float fCarrier;
-    volatile float fSig;
-    volatile float mVoltage;
-    volatile bool ThiEnable;
-    volatile bool SvmEnable;
+  volatile float fCarrier;
+  volatile float fSig;
+  volatile float mVoltage;
+  volatile bool ThiEnable;
+  volatile bool SvmEnable;
 } PulseModeReference;
 
 // 電圧補償係数計算
 float calculate_voltage_coefficient(PulseModeReference pmref) {
-    float voltage_coefficient = 4.0f / M_PI;
-    if (pmref.SvmEnable == false && pmref.ThiEnable == false) {
-        voltage_coefficient = pmref.mVoltage < 0.786f ? 4.0f / M_PI : 4.0f / M_PI + pow(2.267303569733523f * CORRECTIONFACTOR_NORMAL[(byte)min(213, (pmref.mVoltage - 0.786f) * 1000)] / 255, 3);
-    }
-    else if (pmref.SvmEnable == false && pmref.ThiEnable == true) {
-        voltage_coefficient = pmref.mVoltage < 0.906f ? 4.0f / M_PI : 4.0f / M_PI + pow(1.9451517598583832f * CORRECTIONFACTOR_THI[(byte)min(92, (pmref.mVoltage - 0.907f) * 1000)] / 255, 3);
-    }
-    else if (pmref.SvmEnable == true && pmref.ThiEnable == false) {
-        voltage_coefficient = pmref.mVoltage < 0.906f ? 4.0f / M_PI : 4.0f / M_PI + pow(1.9439334827663977f * CORRECTIONFACTOR_SVM[(byte)min(92, (pmref.mVoltage - 0.907f) * 1000)] / 255, 3);
-    }
-    return voltage_coefficient;
+  float voltage_coefficient = 4.0f / M_PI;
+  if (pmref.SvmEnable == false && pmref.ThiEnable == false) {
+    voltage_coefficient = pmref.mVoltage < 0.786f ? 4.0f / M_PI : 4.0f / M_PI + pow(2.267303569733523f * CORRECTIONFACTOR_NORMAL[(byte)min(213, (pmref.mVoltage - 0.786f) * 1000)] / 255, 3);
+  } else if (pmref.SvmEnable == false && pmref.ThiEnable == true) {
+    voltage_coefficient = pmref.mVoltage < 0.906f ? 4.0f / M_PI : 4.0f / M_PI + pow(1.9451517598583832f * CORRECTIONFACTOR_THI[(byte)min(92, (pmref.mVoltage - 0.907f) * 1000)] / 255, 3);
+  } else if (pmref.SvmEnable == true && pmref.ThiEnable == false) {
+    voltage_coefficient = pmref.mVoltage < 0.906f ? 4.0f / M_PI : 4.0f / M_PI + pow(1.9439334827663977f * CORRECTIONFACTOR_SVM[(byte)min(92, (pmref.mVoltage - 0.907f) * 1000)] / 255, 3);
+  }
+  return voltage_coefficient;
 }
 
 void UpdatePwmMode(PulseModeReference pmref, pwm_config* pm) {
-    pmref.mVoltage = max(min(pmref.mVoltage, 1), 0);
-    pm->carrier_freq_hz = pmref.fCarrier;
-    pm->signal_freq_hz = pmref.fSig;
-    pm->modulation_index = pmref.mVoltage * calculate_voltage_coefficient(pmref) / (2 * 127);
-    pm->sig_mode = min(pmref.SvmEnable * 2 + pmref.ThiEnable * 1, 2);
+  pmref.mVoltage = max(min(pmref.mVoltage, 1), 0);
+  pm->carrier_freq_hz = min(pmref.fCarrier, MAX_FC);
+  pm->signal_freq_hz = pmref.fSig;
+  pm->modulation_index = pmref.mVoltage * calculate_voltage_coefficient(pmref) / (2 * 127);
+  pm->sig_mode = min(pmref.SvmEnable * 2 + pmref.ThiEnable * 1, 2);
 
-    uint16_t top_val = 0;
-    if (pm->carrier_freq_hz > 0) {
-        top_val = (uint16_t)(TIMER_CLOCK / (2.0f * pm->carrier_freq_hz));
-    }
-    pm->top = top_val;
+  uint16_t top_val = 0;
+  if (pm->carrier_freq_hz > 0) {
+    top_val = (uint16_t)(TIMER_CLOCK / (2.0f * pm->carrier_freq_hz));
+  }
+  pm->top = top_val;
 
-    uint32_t increment = 0;
-    if (pm->carrier_freq_hz > 0) {
-        increment =
-            (uint32_t)((pm->signal_freq_hz / (2.0f * pm->carrier_freq_hz)) * SIN_LENGTH * SHIFT);
-    }
-    pm->increment = increment;
+  uint32_t increment = 0;
+  if (pm->carrier_freq_hz > 0) {
+    increment =
+      (uint32_t)((pm->signal_freq_hz / (2.0f * pm->carrier_freq_hz)) * SIN_LENGTH * SHIFT);
+    //(uint32_t)((pm->signal_freq_hz / (1000)) * SIN_LENGTH * SHIFT);
+  }
+  pm->increment = increment;
 }
 
 #endif  // PWM_CONTROLLER_H
